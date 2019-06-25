e079bcde1f3da85965691f042580354262ec307d
YoungSang Yun <saegyul@gmail.com>
2019-06-24 19:20:35 -0700

    Initial Commit

---
+++ CamScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CamScanner
+//
+//  Created by Youngsang Yun on 6/24/19.
+//  Copyright © 2019 Youngsang Yun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
